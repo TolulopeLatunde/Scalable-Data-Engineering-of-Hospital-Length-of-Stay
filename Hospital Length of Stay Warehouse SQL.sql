583cf1045025781b136c976d838dd1db56ff64a0
TolulopeLatunde <114585179+TolulopeLatunde@users.noreply.github.com>
2024-10-20 18:44:29 +0100

    Add files via upload

---
+++ Hospital Length of Stay Warehouse SQL.sql
@@ -0,0 +1,11 @@
+USE HospitalLengthOfStayDW;
+
+SELECT * FROM Fact_Stay LIMIT 10;
+
+SELECT * FROM Dim_Patient LIMIT 10;
+
+SELECT * FROM dim_patient LIMIT 10;
+
+SELECT * FROM dim_diagnosis LIMIT 10;
+
+SELECT * FROM dim_facility LIMIT 10;
